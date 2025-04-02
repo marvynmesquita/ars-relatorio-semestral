53a90bb90e31c739a87e61b7da288b57963e869e
Marvyn Mesquita <31486769+marvynmesquita@users.noreply.github.com>
2025-04-01 21:01:14 -0300

    chore: leitura de arquivo XLSX

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const reader = new FileReader();
+
+const db = '/assets/db/matrizESW.xlsx';
+
+async function loadData() {
+    const response = await fetch(db);
+    const data = await response.arrayBuffer();
+    const workbook = XLSX.read(data, { type: 'array' });
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+    const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+    console.log(jsonData);
+} // loadData
+
+loadData();
