2f0ac567d3995a3cfc891801068c8d142c8ecddb
¨Marvyn <marvynmesquita@gmail.com>
2025-04-04 12:47:35 -0300

    chore: adicionado select de períodos

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,11 @@ const reader = new FileReader();
 
 const db = '/assets/db/matrizESW.xlsx';
 
+var periodos = []
+var materias = []
+
+var periodoSelect = document.querySelector('.periodoSelect')
+
 async function loadData() {
     const response = await fetch(db);
     const data = await response.arrayBuffer();
@@ -10,7 +15,25 @@ async function loadData() {
     const worksheet = workbook.Sheets[sheetName];
     const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
 
-    jsonData.forEach((row) => { console.log(row); });
+    jsonData.forEach((row) => {
+        if (row[0][0] != 'P') {
+            if (periodos.includes(row[0][0]) == false) {
+                periodos.push(row[0][0])
+            }
+            if (materias.includes(row[1]) == false) {
+                materias.push(row[1])
+            }
+        }
+
+    });
+
+    if (periodos) {
+        periodos.forEach((periodo) => {
+            const option = new Option(periodo, periodo)
+            periodoSelect.add(option)
+        })
+    console.log(periodoSelect)
+}
 } // loadData
 
 loadData();
