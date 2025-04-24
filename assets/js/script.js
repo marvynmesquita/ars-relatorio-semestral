29d730c6014e3602c0d0ea73d1f8d3d1ac14bec9
Marvyn Mesquita <marvynmesquita@gmail.com>
2025-04-24 20:22:46 -0300

    chore: evento mudando select ao lado

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,16 @@ const db = 'https://ars-db.onrender.com';
 var periodos = []
 var materias = []
 
-var periodoSelect = document.querySelector('.periodoSelect')
+var rows = document.getElementById('horarios-table').getElementsByTagName('tbody')[0].rows
+rows = Array.from(rows)
+rows.forEach((row) => {
+    const periodoSelect = row.querySelector('.periodoSelect')
+    loadData(periodoSelect)
+}
+)
 
 
-async function loadData() {
+async function loadData(periodoSelect) {
     const response = await axios.get(db);
     const data = await response.data;
     
@@ -30,20 +36,23 @@ async function loadData() {
             periodoSelect.add(option)
         })
 }
-} // loadData
+}
 
-loadData();
-
-const materiasFill = () => {
-    var materiaCell = document.querySelector('.materiaCell')
-    materiaCell.innerHTML = ''
+const materiasFill = (childEl) => {
+    const thisParentEl = childEl.parentElement
+    const parentEl = thisParentEl.parentElement
+    const parentId = parentEl.id
+    const parentRow = document.getElementById(parentId)
+    var materiaCell = parentRow.querySelector('.materiaCell')
+    if (materiaCell !== null) {
+        materiaCell.innerHTML = ''
+    }
     var materiaSelect = document.createElement('select')
     materiaSelect.innerHTML = "<option value='' selected disabled></option>"
-    const periodoEscolhido = periodoSelect.value
     materias.forEach((materia) => {
-        if (materia[0] === periodoEscolhido) {
-            const matopt = new Option(materia[1],materia[1])
-            materiaSelect.add(matopt)
+        if (childEl.value == materia[0]) {
+            const option = new Option(materia[1], materia[1])
+            materiaSelect.add(option)
         }
     })
     materiaCell.appendChild(materiaSelect)
@@ -77,20 +86,23 @@ function addTableRow() {
         const option = new Option(periodo, periodo);
         periodoSelect.add(option);
     });
+    periodoSelect.name = `periodo`;
     periodoSelect.classList.add('periodoSelect');
-    periodoSelect.setAttribute('onchange', 'materiasFill()');
+    periodoSelect.setAttribute('onchange', 'materiasFill(this )');
     const cell2 = newRow.insertCell(1);
     
     cell2.appendChild(periodoSelect);
     
     // Célula Disciplina (vazia)
-    newRow.insertCell(2);
-    
+    const cell3 = newRow.insertCell(2);
+    cell3.classList.add('materiaCell');
+    cell3.attributes['required'] = true;
+
     // Célula Situação
     const cell4 = newRow.insertCell(3);
-    cell4.innerHTML = `<input type="text" name="situacao_${rowCount + 1}" maxlength="1" placeholder="Situação">`;
+    cell4.innerHTML = `<input type="text" name="situacao" maxlength="1" placeholder="Situação">`;
     
     // Célula Professor
     const cell5 = newRow.insertCell(4);
-    cell5.innerHTML = `<input type="text" name="professor_${rowCount + 1}" placeholder="Professor">`;
+    cell5.innerHTML = `<input type="text" name="professor" placeholder="Professor">`;
 }
